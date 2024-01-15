93a39d08006c8a799d15f37509b92fda18eb746d
shravanss <shravanappu2001@gmail.com>
2024-01-15 16:30:22 +0530

    nav bar updated

--- script/navbarHandler.js
+++ script/navbarHandler.js
@@ -2,18 +2,18 @@
 
 const menuButton=document.querySelector('.menu-button');
 const menuCloseButton=document.querySelector('.menu-close');
-const navBar=document.querySelector('.nav-background');
+const navBar=document.querySelector('.index-nav-res-container');
 
 menuButton.addEventListener("click", function(){
     console.log("Clicked")
     menuButton.classList.add('hidden');
     menuCloseButton.classList.remove('hidden');
-    navBar.style.display = "flex";
+    navBar.classList.remove('hidden');
 });
 
 menuCloseButton.addEventListener("click", function(){
     console.log("Clicked")
     menuCloseButton.classList.add('hidden');
     menuButton.classList.remove('hidden');
-    navBar.style.display = "none";
+    navBar.classList.add('hidden');
 });
